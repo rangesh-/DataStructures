2146cb9e376f299383b954e9117d7543d1bf53ee
rangesh- <rsripathi781@gmail.com>
2014-06-21 18:46:24 +0530

    Basic DataStructures In C#

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+
+namespace DataStructures
+{
+    class LinkedList<T>
+    {
+        public T value { get; set; }
+        public LinkedList<T> NextList { get; set; }
+
+        public static void Print(LinkedList<T> list)
+        {
+            while (list != null)
+            {
+                Console.WriteLine("Node Value"+list.value);
+                list = list.NextList;
+            }
+        }
+    }
+
+
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list1=new LinkedList<int>();
+            LinkedList<int> list2 = new LinkedList<int>();
+            LinkedList<int> list3 = new LinkedList<int>();
+           
+            list1.value = 1;
+            list1.NextList = list2;
+
+            list2.value = 2;
+            list2.NextList = list3;
+             
+            list3.value = 3;
+           
+            LinkedList<int>.Print(list1);
+        }
+    }
+}
